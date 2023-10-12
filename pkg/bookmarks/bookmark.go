2a08ad70de6c4c0acf335f135b9264279e7c7361
Ulderico Cirello <uldericofilho@gmail.com>
2023-10-11 17:03:03 -0700

    alreadyread: enforce import path

--- pkg/bookmarks/bookmark.go
+++ pkg/bookmarks/bookmark.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package bookmarks
+package bookmarks // import "cirello.io/alreadyread/pkg/bookmarks"
 
 import (
 	"fmt"
