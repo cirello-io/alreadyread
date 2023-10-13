6e8acc3534842a5f9852a71f3ad69d6efea6878a
Ulderico Cirello <uldericofilho@gmail.com>
2023-10-13 16:55:06 -0700

    alreadyread: reorganize the tests

--- pkg/bookmarks/bookmarks_test.go
+++ pkg/bookmarks/bookmarks_test.go
@@ -20,43 +20,37 @@ import (
 	"testing"
 )
 
-func TestBookmarks_DeleteByID(t *testing.T) {
-	type args struct {
-		repository Repository
-		id         int64
+func TestBadURLError(t *testing.T) {
+	errWrap := errors.New("wrapped error")
+	type fields struct {
+		cause  error
+		target error
 	}
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+		name       string
+		fields     fields
+		wantError  string
+		wantUnwrap error
+		wantIs     bool
 	}{
-		{
-			"badDelete",
-			args{
-				repository: &RepositoryMock{
-					DeleteByIDFunc: func(id int64) error {
-						return errors.New("mocked error")
-					},
-				},
-				id: 1},
-			true,
-		},
-		{
-			"goodDelete",
-			args{
-				repository: &RepositoryMock{
-					DeleteByIDFunc: func(id int64) error {
-						return nil
-					},
-				},
-				id: 1},
-			false,
-		},
+		{"nil", fields{}, "invalid URL: <nil>", nil, false},
+		{"Is/right", fields{target: &BadURLError{}}, "invalid URL: <nil>", nil, true},
+		{"Is/wrong", fields{target: errors.New("")}, "invalid URL: <nil>", nil, false},
+		{"Unwrap", fields{cause: errWrap}, "invalid URL: wrapped error", errWrap, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := New(tt.args.repository).DeleteByID(tt.args.id); (err != nil) != tt.wantErr {
-				t.Errorf("DeleteByID() error = %v, wantErr %v", err, tt.wantErr)
+			b := BadURLError{
+				cause: tt.fields.cause,
+			}
+			if got := b.Error(); got != tt.wantError {
+				t.Errorf("BadURLError.Error() = %v, want %v", got, tt.wantError)
+			}
+			if got := b.Unwrap(); !errors.Is(got, tt.wantUnwrap) {
+				t.Errorf("BadURLError.Unwrap() = %v, want %v", got, tt.wantUnwrap)
+			}
+			if got := b.Is(tt.fields.target); got != tt.wantIs {
+				t.Errorf("BadURLError.Is() = %v, want %v", got, tt.wantIs)
 			}
 		})
 	}
@@ -95,37 +89,43 @@ func TestBookmarks_Insert(t *testing.T) {
 	}
 }
 
-func TestBadURLError(t *testing.T) {
-	errWrap := errors.New("wrapped error")
-	type fields struct {
-		cause  error
-		target error
+func TestBookmarks_DeleteByID(t *testing.T) {
+	type args struct {
+		repository Repository
+		id         int64
 	}
 	tests := []struct {
-		name       string
-		fields     fields
-		wantError  string
-		wantUnwrap error
-		wantIs     bool
+		name    string
+		args    args
+		wantErr bool
 	}{
-		{"nil", fields{}, "invalid URL: <nil>", nil, false},
-		{"Is/right", fields{target: &BadURLError{}}, "invalid URL: <nil>", nil, true},
-		{"Is/wrong", fields{target: errors.New("")}, "invalid URL: <nil>", nil, false},
-		{"Unwrap", fields{cause: errWrap}, "invalid URL: wrapped error", errWrap, false},
+		{
+			"badDelete",
+			args{
+				repository: &RepositoryMock{
+					DeleteByIDFunc: func(id int64) error {
+						return errors.New("mocked error")
+					},
+				},
+				id: 1},
+			true,
+		},
+		{
+			"goodDelete",
+			args{
+				repository: &RepositoryMock{
+					DeleteByIDFunc: func(id int64) error {
+						return nil
+					},
+				},
+				id: 1},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := BadURLError{
-				cause: tt.fields.cause,
-			}
-			if got := b.Error(); got != tt.wantError {
-				t.Errorf("BadURLError.Error() = %v, want %v", got, tt.wantError)
-			}
-			if got := b.Unwrap(); !errors.Is(got, tt.wantUnwrap) {
-				t.Errorf("BadURLError.Unwrap() = %v, want %v", got, tt.wantUnwrap)
-			}
-			if got := b.Is(tt.fields.target); got != tt.wantIs {
-				t.Errorf("BadURLError.Is() = %v, want %v", got, tt.wantIs)
+			if err := New(tt.args.repository).DeleteByID(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("DeleteByID() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
